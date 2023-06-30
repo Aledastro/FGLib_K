079c3abc72dc7804a6d0e4a675b9d6c77b8dbf0e
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2023-06-30 21:40:38 +0300

    FGLib v.0.5.12.4 [K]

--- FGLib_K/code/com/uzery/fglib/utils/math/geom/Direct.kt
+++ FGLib_K/code/com/uzery/fglib/utils/math/geom/Direct.kt
@@ -2,6 +2,7 @@ package com.uzery.fglib.utils.math.geom
 
 import com.uzery.fglib.utils.data.debug.DebugData
 import com.uzery.fglib.utils.math.num.IntI
+import kotlin.math.sign
 
 enum class Direct(private val x: Int, private val y: Int) {
     UP(0, -1), RIGHT(1, 0), DOWN(0, 1), LEFT(-1, 0),
@@ -11,18 +12,23 @@ enum class Direct(private val x: Int, private val y: Int) {
 
     operator fun plus(dir: Direct) = from(x + dir.x, y + dir.y)
 
-    private fun from(x: Int, y: Int): Direct {
-        when(IntI(x.coerceIn(-1, 1), y.coerceIn(-1, 1))) {
-            IntI(-1, -1) -> return UP_LEFT
-            IntI(0, -1) -> return UP
-            IntI(1, -1) -> return UP_RIGHT
-            IntI(-1, 0) -> return LEFT
-            IntI(0, 0) -> return CENTER
-            IntI(1, 0) -> return RIGHT
-            IntI(-1, 1) -> return DOWN_LEFT
-            IntI(0, 1) -> return DOWN
-            IntI(1, 1) -> return DOWN_RIGHT
+    companion object{
+        fun from(x: Int, y: Int): Direct {
+            when(IntI(x.coerceIn(-1, 1), y.coerceIn(-1, 1))) {
+                IntI(-1, -1) -> return UP_LEFT
+                IntI(0, -1) -> return UP
+                IntI(1, -1) -> return UP_RIGHT
+                IntI(-1, 0) -> return LEFT
+                IntI(0, 0) -> return CENTER
+                IntI(1, 0) -> return RIGHT
+                IntI(-1, 1) -> return DOWN_LEFT
+                IntI(0, 1) -> return DOWN
+                IntI(1, 1) -> return DOWN_RIGHT
+            }
+            throw DebugData.error("from: $x $y")
         }
-        throw DebugData.error("from: $x $y")
+        fun from(x: Double, y: Double): Direct {
+            return from(x.sign.toInt(),y.sign.toInt())
+        }
     }
 }
