2aefda4524520458d0c10b92c2b6cbb011061e6c
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2023-08-14 18:45:49 +0300

    FGLib v.0.6.9.1 [K]

--- FGLib_K/code/com/uzery/fglib/utils/math/geom/Direct.kt
+++ FGLib_K/code/com/uzery/fglib/utils/math/geom/Direct.kt
@@ -8,7 +8,8 @@ enum class Direct(private val x: Int, private val y: Int) {
     UP(0, -1), RIGHT(1, 0), DOWN(0, 1), LEFT(-1, 0),
     UP_RIGHT(1, -1), UP_LEFT(-1, -1), DOWN_RIGHT(1, 1), DOWN_LEFT(-1, 1), CENTER(0, 0);
 
-    val p = PointN(x.toDouble(), y.toDouble())
+    val value = PointN(x.toDouble(), y.toDouble())
+    val p = value/value.length()
 
     operator fun plus(dir: Direct) = from(x+dir.x, y+dir.y)
 
