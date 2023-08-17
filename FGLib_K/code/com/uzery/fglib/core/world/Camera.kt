a12f77fb1765ddf5717c827150b9503ee22a7e8f
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2023-08-17 23:03:07 +0300

    FGLib v.0.6.11.2 [K]

--- FGLib_K/code/com/uzery/fglib/core/world/Camera.kt
+++ FGLib_K/code/com/uzery/fglib/core/world/Camera.kt
@@ -1,11 +1,12 @@
 package com.uzery.fglib.core.world
 
 import com.uzery.fglib.core.obj.GameObject
+import com.uzery.fglib.core.program.Platform
 import com.uzery.fglib.utils.math.geom.PointN
 
 abstract class Camera: GameObject() {
 
-    open fun drawPOS() = stats.POS
+    open fun drawPOS() = stats.POS-Platform.CANVAS/2
     abstract fun move(p: PointN)
 
     final override fun setValues() {
