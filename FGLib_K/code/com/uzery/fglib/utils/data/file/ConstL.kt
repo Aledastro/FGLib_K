d3effd2bab127e4377c484bcfaa47db2cabfcfc6
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2023-09-18 18:02:25 +0300

    FGLib v.0.8.1.3 [K]

--- FGLib_K/code/com/uzery/fglib/utils/data/file/ConstL.kt
+++ FGLib_K/code/com/uzery/fglib/utils/data/file/ConstL.kt
@@ -3,7 +3,7 @@ package com.uzery.fglib.utils.data.file
 interface ConstL {
     companion object {
         var RUN_JAR = false
-        const val LITTLE = 0.0_000_001
+        const val LITTLE = 0.00_001
 
         const val BUFFER_FORMAT = "UTF-8"
     }
