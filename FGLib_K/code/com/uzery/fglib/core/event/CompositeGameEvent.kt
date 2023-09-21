1606e106a2867b5c011c701033de9af9a415e281
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2023-09-21 19:08:31 +0300

    FGLib v.0.8.1.9 [K]

--- FGLib_K/code/com/uzery/fglib/core/event/CompositeGameEvent.kt
+++ FGLib_K/code/com/uzery/fglib/core/event/CompositeGameEvent.kt
@@ -3,28 +3,28 @@ package com.uzery.fglib.core.event
 import com.uzery.fglib.utils.data.debug.DebugData
 import java.util.*
 
-abstract class CompositeGameEvent: GameEvent() {
-    private val events = LinkedList<GameEvent>()
+open class CompositeGameEvent(vararg events: GameEvent): GameEvent() {
     private var current: GameEvent? = null
+
+    private val events_list = LinkedList(events.toList())
 
     init {
         addAbility {
-            if(current == null && events.isEmpty()){
+            if(current == null && events_list.isEmpty()){
                 setValues()
                 throw DebugData.error("ERROR: empty composite game event: $name")
             }
 
-
-            if (event_time > 0 && (current == null || current!!.wasReadyAndEnds() && events.isNotEmpty())) {
-                current = events.removeFirst()
+            if (event_time > 0 && (current == null || current!!.wasReadyAndEnds() && events_list.isNotEmpty())) {
+                current = events_list.removeFirst()
                 produce(current!!)
             }
         }
     }
 
+    fun add(event: GameEvent) = events_list.addLast(event)
+
     final override fun ready() = true
 
-    final override fun ends() = events.isEmpty() && (current?.wasReadyAndEnds() ?: false)
-
-    protected fun add(event: GameEvent) = events.addLast(event)
+    final override fun ends() = events_list.isEmpty() && (current?.wasReadyAndEnds() ?: false)
 }
--- FGLib_K/code/com/uzery/fglib/core/event/ParallelGameEvent.kt
+++ FGLib_K/code/com/uzery/fglib/core/event/ParallelGameEvent.kt
@@ -2,19 +2,19 @@ package com.uzery.fglib.core.event
 
 import java.util.*
 
-abstract class ParallelGameEvent: GameEvent() {
-    private val events = LinkedList<GameEvent>()
+open class ParallelGameEvent(vararg events: GameEvent): GameEvent() {
+
+    private val events_list = LinkedList(events.toList())
 
     final override fun ready() = true
 
-    final override fun ends() = events.all { it.wasReadyAndEnds() }
+    final override fun ends() = events_list.all { it.wasReadyAndEnds() }
 
-    protected fun add(event: GameEvent) = events.addLast(event)
+    fun add(event: GameEvent) = events_list.addLast(event)
 
-    final override fun start() {
-        events.forEach { produce(it) }
+    init {
+        addAbility {
+            if (object_time == 0) events_list.forEach { produce(it) }
+        }
     }
-
-    override fun update() {}
-    override fun finish() {}
 }
