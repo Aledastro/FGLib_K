ed764ce04a4460d891d76883c94602c89ab25c02
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2023-10-01 18:50:26 +0300

    FGLib v.0.8.8.1 [K]

--- FGLib_K/code/com/uzery/fglib/core/program/Launcher.kt
+++ FGLib_K/code/com/uzery/fglib/core/program/Launcher.kt
@@ -3,16 +3,14 @@ package com.uzery.fglib.core.program
 import javafx.application.Application
 import javafx.stage.Stage
 
-class Launcher {
-    companion object{
-        fun startProcess(options: LaunchOptions, vararg ets: Extension) {
-            Program.initWith(options, *ets)
-            Application.launch(LauncherApp::class.java)
-        }
+object Launcher {
+    fun startProcess(options: LaunchOptions, vararg ets: Extension) {
+        Program.initWith(options, *ets)
+        Application.launch(LauncherApp::class.java)
+    }
 
-        fun startProcess(vararg ets: Extension) {
-            Program.initWith(LaunchOptions.default, *ets)
-            Application.launch(LauncherApp::class.java)
-        }
+    fun startProcess(vararg ets: Extension) {
+        Program.initWith(LaunchOptions.default, *ets)
+        Application.launch(LauncherApp::class.java)
     }
 }
