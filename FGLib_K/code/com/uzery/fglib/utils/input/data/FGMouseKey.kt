9abc923120753f5ef664e9306877cff8780d2ce6
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2023-12-02 10:30:35 +0300

    FGLib v.0.11.3.1 [K]

--- FGLib_K/code/com/uzery/fglib/utils/input/data/FGMouseKey.kt
+++ FGLib_K/code/com/uzery/fglib/utils/input/data/FGMouseKey.kt
@@ -3,15 +3,18 @@ package com.uzery.fglib.utils.input.data
 import javafx.scene.input.MouseButton
 
 data class FGMouseKey(val value: String, val id: Int) {
-
-
     companion object {
-        val LEFT = from(MouseButton.PRIMARY)
-        val RIGHT = from(MouseButton.SECONDARY)
+        val PRIMARY = from(MouseButton.PRIMARY)
+        val SECONDARY = from(MouseButton.SECONDARY)
         val MIDDLE = from(MouseButton.MIDDLE)
         val BACK = from(MouseButton.BACK)
         val FORWARD = from(MouseButton.FORWARD)
         val NONE = from(MouseButton.NONE)
+
+        val LEFT
+            get() = PRIMARY
+        val RIGHT
+            get() = SECONDARY
 
         private fun from(key: MouseButton): FGMouseKey {
             return FGMouseKey(key.name, key.ordinal)
