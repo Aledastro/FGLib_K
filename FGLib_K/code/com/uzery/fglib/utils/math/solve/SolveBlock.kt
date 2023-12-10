2334b8fbf238503205e9321fee85fe957637e7aa
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2023-12-10 18:18:01 +0300

    FGLib v.0.12.5.0 [K]

--- FGLib_K/code/com/uzery/fglib/utils/math/solve/SolveBlock.kt
+++ FGLib_K/code/com/uzery/fglib/utils/math/solve/SolveBlock.kt
@@ -7,24 +7,53 @@ class SolveBlock(
     private val expression: String,
     private val resolve: (String) -> SolveNumber
 ) {
-    private val operators = arrayListOf(
-        arrayListOf("+", "-"),
-        arrayListOf("*", "/", "%"),
-        arrayListOf("//"),
-        arrayListOf("^"),
-        arrayListOf("|"), //todo
-        arrayListOf("(", ")") //todo
-    )
+    companion object{
+        private val operators = arrayListOf(
+            arrayListOf("+", "-"),
+            arrayListOf("+-"),
+            arrayListOf("*", "/", "%"),
+            arrayListOf("//"),
+            arrayListOf("^"),
+            /*arrayListOf("|"), //todo
+            arrayListOf("(", ")") //todo*/
+        )
+        private val full_ops = ArrayList<String>()
+
+        init {
+            operators.forEach { full_ops.addAll(it) }
+        }
+    }
 
     fun solve(): String {
         return solved().expression
     }
 
-    private lateinit var next_operator: String
-
     private fun solved(): SolveBlock {
         if (operators.all { list -> list.all { it !in expression } }) return this
 
+        fun checkForDoubleOpsOrEndOp(): Boolean{
+            val blocks_un = getBlocks(full_ops)
+            var last = false
+            var last_block_un = ""
+
+            if (blocks_un.size == 1) return false
+
+            if (blocks_un.last() in full_ops) return true
+
+            for (block_un in blocks_un) {
+                val now = block_un in full_ops
+
+                val double_op = last_block_un+block_un !in listOf("+-", "//")
+                if (last && now && double_op) return true
+                last = now
+                last_block_un = block_un
+            }
+
+            return false
+        }
+        if(checkForDoubleOpsOrEndOp()) return ERROR
+
+        var next_operator: String
         for (ops in operators) {
             val blocks = getBlocks(ops)
 
@@ -32,14 +61,22 @@ class SolveBlock(
 
             var result = defaultValue
             next_operator = "+"
-            for (block in blocks) {
-                val solvedBlock = SolveBlock(defaultValue, block, resolve).solved()
-                val exp = solvedBlock.expression
 
-                if (exp in ops) {
-                    next_operator = exp
-                } else {
-                    result = applyOperator(result, resolve(exp), next_operator)
+            for (block in blocks) {
+                val solved_block = SolveBlock(defaultValue, block, resolve).solved()
+
+                when (val exp = solved_block.expression) {
+                    ERROR_EXPRESSION -> {
+                        return ERROR
+                    }
+
+                    in ops -> {
+                        next_operator = exp
+                    }
+
+                    else -> {
+                        result = applyOperator(result, resolve(exp), next_operator)
+                    }
                 }
             }
             return SolveBlock(defaultValue, result.expression, resolve)
@@ -84,4 +121,8 @@ class SolveBlock(
         }
         return res
     }
+
+    private val ERROR_EXPRESSION = "ERROR"
+    private val ERROR: SolveBlock
+        get() = SolveBlock(this.defaultValue, ERROR_EXPRESSION, this.resolve)
 }
