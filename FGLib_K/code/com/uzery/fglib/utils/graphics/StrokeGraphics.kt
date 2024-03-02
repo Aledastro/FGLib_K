dec2634d61a7488a882858af4cb81a153f834187
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2024-03-03 02:13:09 +0300

    FGLib v.0.14.1.1 [K]

--- FGLib_K/code/com/uzery/fglib/utils/graphics/StrokeGraphics.kt
+++ FGLib_K/code/com/uzery/fglib/utils/graphics/StrokeGraphics.kt
@@ -1,6 +1,6 @@
 package com.uzery.fglib.utils.graphics
 
-abstract class StrokeGraphics: GeometryGraphics() {
+abstract class StrokeGraphics(agc: AffineGraphics): GeometryGraphics(agc) {
     abstract var width: Double
     abstract fun dashes(offset: Double, vararg dashes: Double)
 }
