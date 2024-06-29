e4779f3824162149926c1138b6419f904c6a52a1
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2024-06-30 01:12:43 +0300

    FGLib v.0.14.14.3 [K]

--- FGLib_K/code/com/uzery/fglib/core/obj/visual/LoadVisualiser.kt
+++ FGLib_K/code/com/uzery/fglib/core/obj/visual/LoadVisualiser.kt
@@ -3,4 +3,4 @@ package com.uzery.fglib.core.obj.visual
 import com.uzery.fglib.core.obj.GroupComponent
 import com.uzery.fglib.core.obj.component.OnLoadComponent
 
-class LoadVisualiser(load: OnLoadComponent, vis: Visualiser): GroupComponent(load, vis)
+open class LoadVisualiser(load: OnLoadComponent, vis: Visualiser): GroupComponent(load, vis)
