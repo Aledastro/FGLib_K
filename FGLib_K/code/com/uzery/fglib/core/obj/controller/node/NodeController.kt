870ba8c7dc98c23daf99854af3289293b2096a73
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2024-10-06 02:29:12 +0300

    FGLib v.0.15.8.2 [K]

--- FGLib_K/code/com/uzery/fglib/core/obj/controller/node/NodeController.kt
+++ FGLib_K/code/com/uzery/fglib/core/obj/controller/node/NodeController.kt
@@ -2,7 +2,10 @@ package com.uzery.fglib.core.obj.controller.node
 
 import com.uzery.fglib.core.obj.controller.GroupController
 
-class NodeController(default: TempActionNode, private vararg val nodes: TempActionNode): GroupController() {
+class NodeController(
+    default: TempActionNode,
+    private vararg val nodes: TempActionNode
+): GroupController() {
     init {
         addController {
             val res = nodes.firstOrNull { node -> node.choose_ready } ?: default
