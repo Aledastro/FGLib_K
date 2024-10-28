e234655933e608cf9f91a7fd680db8ee6b49f98b
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2024-10-28 22:13:57 +0300

    FGLib v.0.15.14.0 [K]

--- FGLib_K/code/com/uzery/fglib/utils/graphics/shader/FGShader.kt
+++ FGLib_K/code/com/uzery/fglib/utils/graphics/shader/FGShader.kt
@@ -3,19 +3,19 @@ package com.uzery.fglib.utils.graphics.shader
 import com.uzery.fglib.core.program.Platform.packager
 import com.uzery.fglib.utils.data.file.TextData
 
-abstract class FGShader(shader_source: ArrayList<String>) {
+class FGShader(shader_source: ArrayList<String>) {
     constructor(filename: String): this(TextData[filename])
 
-    val sh
+    private val shp
         get() = packager.shader!!
 
-    val source: Any = sh.createShader(shader_source)
+    val source: Any = shp.createShader(shader_source)
 
     fun on() {
-        sh.shaderOn(source)
+        shp.shaderOn(source)
     }
 
     fun off() {
-        sh.shaderOff(source)
+        shp.shaderOff(source)
     }
 }
