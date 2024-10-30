1998a24bacf6f7a199d355e18c24b1c2b9282e32
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2024-10-30 13:15:27 +0300

    FGLib v.0.15.14.1 [K]

--- FGLib_K/code/com/uzery/fglib/core/obj/listener/InputAction.kt
+++ FGLib_K/code/com/uzery/fglib/core/obj/listener/InputAction.kt
@@ -8,19 +8,22 @@ data class InputAction(val code: String, val prime: GameObject, private val full
 
     val stats = prime.stats
     val options = ArrayList<String>()
-    val info: String
+    lateinit var info: String
 
     init {
-        info = resolve(full_info)
+        resolve(full_info)
     }
 
-    private fun resolve(info: String): String {
-        if (" | " !in info) return info
+    private fun resolve(info: String) {
+        if (" | " !in info) {
+            this.info = info
+            return
+        }
         val tokenizer = StringTokenizer(FGUtils.subAfter(info, "|"))
         while (tokenizer.hasMoreTokens()) {
             options.add(tokenizer.nextToken())
         }
-        return FGUtils.subBefore(info, "|", -1)
+        this.info = FGUtils.subBefore(info, "|", -1)
     }
 
     override fun toString() = "InputAction: $code - $info - ${prime.name}"
