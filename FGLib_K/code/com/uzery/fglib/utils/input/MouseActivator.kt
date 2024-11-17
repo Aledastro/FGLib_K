2530ff5ee21ab6faa43fda7105381c257bf40f61
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2024-11-17 20:13:43 +0300

    FGLib v.0.16.3.1 [K]

--- FGLib_K/code/com/uzery/fglib/utils/input/MouseActivator.kt
+++ FGLib_K/code/com/uzery/fglib/utils/input/MouseActivator.kt
@@ -1,5 +1,6 @@
 package com.uzery.fglib.utils.input
 
+import com.uzery.fglib.core.program.Platform
 import com.uzery.fglib.core.program.Platform.graphics
 import com.uzery.fglib.utils.input.data.FGMouseKey
 import com.uzery.fglib.utils.math.geom.PointN
@@ -9,7 +10,7 @@ abstract class MouseActivator {
     protected abstract fun pos0(): PointN
     protected abstract fun scroll0(): PointN
     val pos: PointN
-        get() = pos0()/graphics.scale
+        get() = pos0()/Platform.scale
 
     val scroll: Int
         get() = sign(scroll0().Y).toInt()
