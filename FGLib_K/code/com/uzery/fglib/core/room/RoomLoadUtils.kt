9976cd3f4fe894b006fd8141e4497ce6b5491025
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2024-11-18 15:39:05 +0300

    FGLib v.0.16.4.3 [K]

--- FGLib_K/code/com/uzery/fglib/core/room/RoomLoadUtils.kt
+++ FGLib_K/code/com/uzery/fglib/core/room/RoomLoadUtils.kt
@@ -1,7 +1,6 @@
-package com.uzery.fglib.core.world
+package com.uzery.fglib.core.room
 
 import com.uzery.fglib.core.obj.GameObject
-import com.uzery.fglib.core.room.Room
 import com.uzery.fglib.utils.FGUtils
 import com.uzery.fglib.utils.data.file.TextData
 import com.uzery.fglib.utils.data.getter.AbstractClassGetter
@@ -9,18 +8,14 @@ import com.uzery.fglib.utils.data.getter.ClassGetter
 import com.uzery.fglib.utils.math.geom.PointN
 import java.util.*
 
-object WorldLoadUtils {
+object RoomLoadUtils {
     val room_info_cg = object: ClassGetter<Pair<PointN, PointN>>() {
         override fun addAll() {
             add("room", 2) { Pair(pos, size) }
         }
     }
 
-    fun readRoom(getter: AbstractClassGetter<GameObject>, filename: String): Room {
-        return readRoom(getter, TextData[filename])
-    }
-
-    fun readRoom(getter: AbstractClassGetter<GameObject>, input: List<String>): Room {
+    fun makeRoom(getter: AbstractClassGetter<GameObject>, input: List<String>): Room {
         val list = LinkedList<String>()
         list.addAll(input)
 
@@ -42,13 +37,17 @@ object WorldLoadUtils {
         return Room(room_info.first, room_info.second, objects)
     }
 
+    fun readRoom(getter: AbstractClassGetter<GameObject>, filename: String): Room {
+        return makeRoom(getter, TextData[filename])
+    }
+
     fun readRooms(getter: AbstractClassGetter<GameObject>, filenames: Array<String>): Array<Room> {
         return Array(filenames.size) { i ->
             readRoom(getter, filenames[i])
         }
     }
 
-    fun readRoomsFrom(getter: AbstractClassGetter<GameObject>, dir: String): Array<Room> {
+    fun readAllFrom(getter: AbstractClassGetter<GameObject>, dir: String): Array<Room> {
         return readRooms(getter, TextData.filesFrom(dir).toTypedArray())
     }
 }
