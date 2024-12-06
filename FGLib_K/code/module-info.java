d4dc2e9788981c470d29dc469956147d8e1641f3
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2024-12-06 07:31:20 +0300

    FGLib v.0.17.1.1 [K]

--- FGLib_K/code/module-info.java
+++ FGLib_K/code/module-info.java
@@ -9,6 +9,7 @@ module FGLib_K {
     exports com.uzery.fglib.core.component.controller;
     exports com.uzery.fglib.core.component.controller.node;
     exports com.uzery.fglib.core.component.listener;
+    exports com.uzery.fglib.core.component.load;
     exports com.uzery.fglib.core.component.property;
     exports com.uzery.fglib.core.component.reaction;
     exports com.uzery.fglib.core.component.visual;
