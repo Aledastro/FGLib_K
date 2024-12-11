c9a6c9070cdc8b684ded1d180c2d552e6c7d5f77
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2024-12-12 00:32:47 +0300

    FGLib v.0.17.3.1 [K]

--- FGLib_K/code/module-info.java
+++ FGLib_K/code/module-info.java
@@ -1,5 +1,41 @@
 /**
- * TODO("doc")
+ * FGLib is free open-source Kotlin game library, focused on developing 2D-games
+ * <p>
+ * Pros:
+ * <p>
+ * (*) Easy to Learn, Hard to Master
+ * <p>
+ * FGLib provides intuitive environment to work with and it easy to build simple games in it.
+ * However, it has many powerful features if you know it well
+ * <p>
+ * (*) Open-Source and Free
+ * <p>
+ * Due to it open-source nature, you can modify FGLib functionality. And it not that hard,
+ * because FGLib as your project is just raw kotlin project
+ * <p>
+ * (*) Powerful RoomEditor
+ * <p>
+ * Allows you to make levels easily
+ * <p>
+ * (*) Technology-Independent
+ * <p>
+ * FGLib is technology-independent and more about philosophy about how to make games.
+ * It has different realisations for different purposes,
+ * such as simple JavaAwt for lightweight apps, raw OpenGL for complex games or JavaFX, that keeps balance.
+ * And all works in unite environment. You can make your own realisation as well!
+ * <p>
+ * Cons:
+ * <p>
+ * (*) Many features are still in development, because I not need them currently
+ * <p>
+ * (*) Lack of documentation, but you can have developer support
+ * <p>
+ * (*) Limited in some ways
+ * <p>
+ * Developer: Uzery Studio (Alexander Kandrashkin)
+ * <p>
+ * From one who love coding, to ones who love coding!
+ * Join the community and make FGLib bigger!
  **/
 module FGLib_K {
     requires kotlin.stdlib;
