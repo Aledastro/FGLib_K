8929382f0caf589af2f2dce386043947acfe152e
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2024-12-16 20:54:01 +0300

    FGLib v.0.17.3.10 [K]

--- FGLib_K/code/com/uzery/fglib/core/component/property/GroupProperty.kt
+++
@@ -1,9 +0,0 @@
-package com.uzery.fglib.core.component.property
-
-import com.uzery.fglib.core.component.GroupComponent
-import com.uzery.fglib.core.component.ObjectComponent
-
-/**
- * TODO("doc")
- **/
-abstract class GroupProperty(vararg component: ObjectComponent): GroupComponent(*component)
