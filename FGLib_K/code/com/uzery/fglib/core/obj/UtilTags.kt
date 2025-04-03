3dd3fd52e454ba69a7dfd81f4f8944c2d8026c49
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2025-04-04 01:35:02 +0300

    FGLib v.0.18.1.1 [K]

--- FGLib_K/code/com/uzery/fglib/core/obj/UtilTags.kt
+++ FGLib_K/code/com/uzery/fglib/core/obj/UtilTags.kt
@@ -1,7 +1,7 @@
 package com.uzery.fglib.core.obj
 
 object UtilTags {
-    val util_immovable = "util_immovable"
-    val util_inactive = "util_inactive"
-    val util_smart_object = "util_smart_object"
+    const val util_immovable = "util_immovable"
+    const val util_inactive = "util_inactive"
+    const val util_smart_object = "util_smart_object"
 }
