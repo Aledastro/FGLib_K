b237dd068fcb5cd9130646b72669d23537cded7f
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2025-04-04 15:19:28 +0300

    FGLib v.0.18.1.4 [K]

--- FGLib_K/code/com/uzery/fglib/utils/data/file/FGLibConst.kt
+++ FGLib_K/code/com/uzery/fglib/utils/data/file/FGLibConst.kt
@@ -1,12 +1,15 @@
 package com.uzery.fglib.utils.data.file
 
-import com.uzery.fglib.utils.FGUtils
+import com.uzery.fglib.utils.graphics.data.FGColor
 
 /**
  * TODO("doc")
  **/
 object FGLibConst {
     const val LITTLE = 0.00_001
+
+    val FGLIB_LIGHT_COLOR = FGColor.web("#dfe0e8")
+    val FGLIB_DARK_COLOR = FGColor.web("#3a3f5e")
 
     const val BUFFER_FORMAT = "UTF-8"
 
