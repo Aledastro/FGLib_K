598751b753824ff92a79e542c7382e244c724ce4
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2025-05-22 04:32:48 +0300

    FGLib v.0.18.12.2 [K]

--- FGLib_K/code/com/uzery/fglib/utils/graphics/render/GraphicsRender.kt
+++ FGLib_K/code/com/uzery/fglib/utils/graphics/render/GraphicsRender.kt
@@ -1,5 +1,9 @@
 package com.uzery.fglib.utils.graphics.render
 
 import com.uzery.fglib.utils.math.geom.PointN
+import com.uzery.fglib.utils.math.geom.shape.RectN
 
-class GraphicsRender(val pos: PointN, val size: PointN)
+class GraphicsRender(val pos: PointN, val size: PointN) {
+    val main = RectN(pos, size)
+    val center = (pos+size)/2
+}
