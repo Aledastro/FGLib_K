9b97df09d480463f082a5c509888248d202aae4d
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2025-05-22 05:48:09 +0300

    FGLib v.0.18.12.3 [K]

--- FGLib_K/code/com/uzery/fglib/utils/graphics/GeometryGraphics.kt
+++ FGLib_K/code/com/uzery/fglib/utils/graphics/GeometryGraphics.kt
@@ -4,6 +4,8 @@ import com.uzery.fglib.utils.graphics.data.FGColor
 import com.uzery.fglib.utils.math.geom.PointN
 import com.uzery.fglib.utils.math.geom.Shape
 import com.uzery.fglib.utils.math.geom.shape.FigureN
+import com.uzery.fglib.utils.math.geom.shape.OvalN
+import com.uzery.fglib.utils.math.geom.shape.RectN
 
 /**
  * TODO("doc")
@@ -16,7 +18,7 @@ abstract class GeometryGraphics(protected val agc: AffineGraphics) {
 
     protected abstract fun oval0(pos: PointN, size: PointN, color: FGColor)
 
-    ///////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
 
     fun rect(pos: PointN, size: PointN, color: FGColor) {
         if (agc.isOutOfBounds(pos, size)) return
@@ -30,21 +32,21 @@ abstract class GeometryGraphics(protected val agc: AffineGraphics) {
         oval0(transform.pos(pos), transform.size(pos, size), agc.getAlphaColor(color))
     }
 
-    fun figure(pos: PointN, fig: FigureN, color: FGColor) {
-        if (!fig.exists()) return
+    fun figure(pos: PointN, figure: FigureN, color: FGColor) {
+        if (!figure.exists()) return
 
         val c = agc.getAlphaColor(color)
 
-        for (field in fig.fields) {
+        for (field in figure.fields) {
             val p = PointN(Array(field.dim) { field.mirage[it, 0] })
             agc.stroke.line(pos, p, c)
         }
-        for (p in fig.current_pos) {
+        for (p in figure.current_pos) {
             oval(pos-PointN(1, 1)+p, PointN(2, 2), c)
         }
     }
 
-    ///////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
 
     fun draw(pos: PointN, shape: Shape, color: FGColor) {
         when (shape.code) {
@@ -54,15 +56,45 @@ abstract class GeometryGraphics(protected val agc: AffineGraphics) {
         }
     }
 
-    ///////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
 
     fun rectL(pos: PointN, size: PointN, color: FGColor) = rect(pos, size, color)
     fun rectC(pos: PointN, size: PointN, color: FGColor) = rect(pos-size/2, size, color)
     fun rectR(pos: PointN, size: PointN, color: FGColor) = rect(pos-size, size, color)
 
+    fun rect(pos: PointN, rect: RectN, color: FGColor) = rect(pos+rect.pos, rect.size, color)
+    fun rectL(pos: PointN, rect: RectN, color: FGColor) = rect(pos+rect.pos, rect.size, color)
+    fun rectC(pos: PointN, rect: RectN, color: FGColor) = rect(pos+rect.pos-rect.size/2, rect.size, color)
+    fun rectR(pos: PointN, rect: RectN, color: FGColor) = rect(pos+rect.pos-rect.size, rect.size, color)
+
+    fun rect(rect: RectN, color: FGColor) = rect(rect.pos, rect.size, color)
+    fun rectL(rect: RectN, color: FGColor) = rect(rect.pos, rect.size, color)
+    fun rectC(rect: RectN, color: FGColor) = rect(rect.pos-rect.size/2, rect.size, color)
+    fun rectR(rect: RectN, color: FGColor) = rect(rect.pos-rect.size, rect.size, color)
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
     fun ovalL(pos: PointN, size: PointN, color: FGColor) = oval(pos, size, color)
     fun ovalC(pos: PointN, size: PointN, color: FGColor) = oval(pos-size/2, size, color)
     fun ovalR(pos: PointN, size: PointN, color: FGColor) = oval(pos-size, size, color)
+
+    fun oval(pos: PointN, oval: OvalN, color: FGColor) = oval(pos+oval.pos, oval.size, color)
+    fun ovalL(pos: PointN, oval: OvalN, color: FGColor) = oval(pos+oval.pos, oval.size, color)
+    fun ovalC(pos: PointN, oval: OvalN, color: FGColor) = oval(pos+oval.pos-oval.size/2, oval.size, color)
+    fun ovalR(pos: PointN, oval: OvalN, color: FGColor) = oval(pos+oval.pos-oval.size, oval.size, color)
+
+    fun oval(oval: OvalN, color: FGColor) = oval(oval.pos, oval.size, color)
+    fun ovalL(oval: OvalN, color: FGColor) = oval(oval.pos, oval.size, color)
+    fun ovalC(oval: OvalN, color: FGColor) = oval(oval.pos-oval.size/2, oval.size, color)
+    fun ovalR(oval: OvalN, color: FGColor) = oval(oval.pos-oval.size, oval.size, color)
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    fun figure(figure: FigureN, color: FGColor) = figure(PointN.ZERO, figure, color)
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
 
     fun drawL(pos: PointN, shape: Shape, color: FGColor) = draw(pos, shape, color)
     fun drawC(pos: PointN, shape: Shape, color: FGColor) = draw(pos-shape.S/2, shape, color)
@@ -73,5 +105,5 @@ abstract class GeometryGraphics(protected val agc: AffineGraphics) {
     fun drawC(shape: Shape, color: FGColor) = draw(-shape.S/2, shape, color)
     fun drawR(shape: Shape, color: FGColor) = draw(-shape.S, shape, color)
 
-    ///////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
 }
