1f064d9280f9695be94c2b088c75a3a08df1d368
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2025-05-22 06:15:18 +0300

    FGLib v.0.18.12.5 [K]

--- FGLib_K/code/com/uzery/fglib/utils/graphics/render/GraphicsRender.kt
+++ FGLib_K/code/com/uzery/fglib/utils/graphics/render/GraphicsRender.kt
@@ -1,9 +1,23 @@
 package com.uzery.fglib.utils.graphics.render
 
+import com.uzery.fglib.core.program.extension.FGLayout
 import com.uzery.fglib.utils.math.geom.PointN
 import com.uzery.fglib.utils.math.geom.shape.RectN
 
 class GraphicsRender(val pos: PointN, val size: PointN) {
     val main = RectN(pos, size)
-    val center = pos+size/2
+
+    private fun of(layout: FGLayout): PointN {
+        return pos+size*layout.value
+    }
+
+    val top_left = of(FGLayout.TOP_LEFT)
+    val top = of(FGLayout.TOP)
+    val top_right = of(FGLayout.TOP_RIGHT)
+    val left = of(FGLayout.LEFT)
+    val center = of(FGLayout.CENTER)
+    val right = of(FGLayout.RIGHT)
+    val bottom_left = of(FGLayout.BOTTOM_LEFT)
+    val bottom = of(FGLayout.BOTTOM)
+    val bottom_right = of(FGLayout.BOTTOM_RIGHT)
 }
