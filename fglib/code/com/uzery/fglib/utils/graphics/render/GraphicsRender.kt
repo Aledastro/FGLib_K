2cdeedbf06ab1d8db8c68305cfc168af94f266be
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2025-06-02 23:47:00 +0300

    FGLib v.0.18.16.0 [K]

--- fglib/code/com/uzery/fglib/utils/graphics/render/GraphicsRender.kt
+++ fglib/code/com/uzery/fglib/utils/graphics/render/GraphicsRender.kt
@@ -9,9 +9,29 @@ import kotlin.math.min
 class GraphicsRender(val pos: PointN, val size: PointN) {
     val draw_area = RectN(pos, size)
 
+    ////////////////////////////////////////////////////////////////////////////////
+
     fun layout(k_pos: PointN): PointN {
         return pos+size*k_pos
     }
+
+    fun layout(kx: Double, ky: Double) = layout(PointN(kx, ky))
+
+    private fun layout(layout: FGLayout) = layout(layout.value)
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+    val TOP_LEFT = layout(FGLayout.TOP_LEFT)
+    val TOP = layout(FGLayout.TOP)
+    val TOP_RIGHT = layout(FGLayout.TOP_RIGHT)
+    val LEFT = layout(FGLayout.LEFT)
+    val CENTER = layout(FGLayout.CENTER)
+    val RIGHT = layout(FGLayout.RIGHT)
+    val BOTTOM_LEFT = layout(FGLayout.BOTTOM_LEFT)
+    val BOTTOM = layout(FGLayout.BOTTOM)
+    val BOTTOM_RIGHT = layout(FGLayout.BOTTOM_RIGHT)
+
+    ////////////////////////////////////////////////////////////////////////////////
 
     fun child(d_pos: PointN, size: PointN, layout: FGLayout = FGLayout.TOP_LEFT): GraphicsRender {
         return GraphicsRender(pos+d_pos-size*layout.value, size)
@@ -24,18 +44,4 @@ class GraphicsRender(val pos: PointN, val size: PointN) {
     fun childFromLayout(k_pos1: PointN, k_pos2: PointN): GraphicsRender {
         return childFrom(layout(k_pos1), layout(k_pos2))
     }
-
-    fun layout(kx: Double, ky: Double) = layout(PointN(kx, ky))
-
-    private fun layout(layout: FGLayout) = layout(layout.value)
-
-    val TOP_LEFT = layout(FGLayout.TOP_LEFT)
-    val TOP = layout(FGLayout.TOP)
-    val TOP_RIGHT = layout(FGLayout.TOP_RIGHT)
-    val LEFT = layout(FGLayout.LEFT)
-    val CENTER = layout(FGLayout.CENTER)
-    val RIGHT = layout(FGLayout.RIGHT)
-    val BOTTOM_LEFT = layout(FGLayout.BOTTOM_LEFT)
-    val BOTTOM = layout(FGLayout.BOTTOM)
-    val BOTTOM_RIGHT = layout(FGLayout.BOTTOM_RIGHT)
 }
