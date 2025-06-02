3eb2d03169b9f385bb9a701ce847f5ea57c02e3d
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2025-06-02 23:44:38 +0300

    FGLib v.0.18.15.1 [K]

--- fglib/code/com/uzery/fglib/utils/graphics/render/GraphicsRender.kt
+++ fglib/code/com/uzery/fglib/utils/graphics/render/GraphicsRender.kt
@@ -3,12 +3,26 @@ package com.uzery.fglib.utils.graphics.render
 import com.uzery.fglib.utils.math.geom.FGLayout
 import com.uzery.fglib.utils.math.geom.PointN
 import com.uzery.fglib.utils.math.geom.shape.RectN
+import kotlin.math.max
+import kotlin.math.min
 
 class GraphicsRender(val pos: PointN, val size: PointN) {
     val draw_area = RectN(pos, size)
 
     fun layout(k_pos: PointN): PointN {
         return pos+size*k_pos
+    }
+
+    fun child(d_pos: PointN, size: PointN, layout: FGLayout = FGLayout.TOP_LEFT): GraphicsRender {
+        return GraphicsRender(pos+d_pos-size*layout.value, size)
+    }
+    fun childFrom(d_pos1: PointN, d_pos2: PointN): GraphicsRender {
+        val min = PointN.transform(d_pos1, d_pos2) { a, b -> min(a, b) }
+        val max = PointN.transform(d_pos1, d_pos2) { a, b -> max(a, b) }
+        return child(min, max-min)
+    }
+    fun childFromLayout(k_pos1: PointN, k_pos2: PointN): GraphicsRender {
+        return childFrom(layout(k_pos1), layout(k_pos2))
     }
 
     fun layout(kx: Double, ky: Double) = layout(PointN(kx, ky))
