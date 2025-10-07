75f89ed03eb6db481a0a2a8514b86649b97098e8
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2025-10-07 17:19:45 +0300

    FGLib v.0.18.27.15 [K]

--- fglib/code/com/uzery/fglib/core/program/extension/Extension.kt
+++ fglib/code/com/uzery/fglib/core/program/extension/Extension.kt
@@ -290,20 +290,6 @@ abstract class Extension(vararg children: Extension) {
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    fun getParent(depth: Int = 1): Extension {
-        if (depth < 1) throw DebugData.error("depth should be positive")
-
-        var current: Extension = this
-        var remained = depth
-        while (true) {
-            current = current.stats.parent ?: throw DebugData.error("No parent found")
-            remained--
-
-            if (remained == 0) return current
-        }
-    }
-
-    @JvmName("getTypedParent")
     inline fun <reified T: Extension> getParent(depth: Int = 1): T {
         if (depth < 1) throw DebugData.error("depth should be positive")
 
