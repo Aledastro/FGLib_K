89765e1d788aab0a5fb5493bc3b8fd27753b7edd
Alexander Kandrashkin <K.AlexanderZ.work@gmail.com>
2025-10-12 11:19:53 +0300

    FGLib v.0.18.27.16 [K]

--- fglib/code/com/uzery/fglib/core/program/FGClipboard.kt
+++ fglib/code/com/uzery/fglib/core/program/FGClipboard.kt
@@ -1,8 +1,11 @@
 package com.uzery.fglib.core.program
 
+import com.uzery.fglib.utils.data.image.FGImage
+
 /**
  * TODO("doc")
  **/
 abstract class FGClipboard {
     abstract var string: String?
+    abstract var image: FGImage?
 }
